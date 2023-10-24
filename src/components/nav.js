ca8fcfacda4b2f30a835bf6d27ac9d8bf5726f1c
henosg53 <henosg53@gmail.com>
2023-10-24 10:24:00 -0400

    adjusted navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,7 +6,7 @@ import { SearchBar } from "./search_bar";
 
 export default function Nav(){
     return(
-        <div  className="flex items-center justify-around bg-gray-200 w-full h-16 gap-5 sm:px-20">
+        <div  className="flex items-center justify-between bg-gray-200 w-full h-16 gap-5 sm:px-5 ">
           
             <div className='flex w-full ' direction={"row"}>
                 <IconButton variant={'contained'} color={'blue'} size={'small'}>
@@ -15,17 +15,17 @@ export default function Nav(){
                 <Typography variant="h4"  fontStyle={'bold'} >Miles</Typography>
                 
             </div>
-            <div className="py-10 w-[60%]" direction={"row"}>
+            <div className="py-10 w-full" direction={"row"}>
                 <SearchBar/>
                 
             </div>
-            <div className="hidden sm:visible md:flex lg:flex flex-row w-full" direction={"row"}>
+            <div className="hidden sm:visible md:flex lg:flex flex-row w-full justify-end" direction={"row"}>
                 <Button  variant="text" style={{color:'gray'}}  endIcon={<Switch/>} disableElevation>Test mode</Button>
                 
                 {/* <Switch/> */}
             </div>
 
-            <div className="flex w-full" direction={"row"}>
+            <div className="flex w-full justify-end" direction={"row"}>
                 <IconButton variant={'contained'} color={''} size={'small'}><NotificationsIcon/></IconButton>
                 <IconButton variant={'contained'} color={''} size={'small'}><PersonOutlineOutlinedIcon/></IconButton>
             </div>
